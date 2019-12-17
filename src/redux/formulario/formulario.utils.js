05e6710e0b4bfc4bc7ba3f2ca01ebd23a5d45784
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-12-17 14:50:47 -0300

    corrigindo erro com toSource()

--- src/redux/formulario/formulario.utils.js
+++ src/redux/formulario/formulario.utils.js
@@ -72,5 +72,20 @@ export const verificaStep = (stepNumber, stepValue) => {
 }
 
 export const isEmptyObject = obj => {
-    return obj.toSource() === "({})";
+    // null é "empty"
+    if (obj == null) return true;
+
+    // Suponhamos que se tenha uma propriedade length com um valor diferente de zero
+    // Essa proriedade será verdadeira
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Caso contrário ela tem todas as sua propriedades?
+    // Isto não se manipula
+    // toString e valueOf são erros de enumeração no IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
 }
