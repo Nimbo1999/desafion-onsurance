6fc5c2d73ee6ae69668d3d60172f56f377137645
Matheus de Oliveira Lopes <matlopes1999@gmail.com>
2019-12-17 00:15:08 -0300

    fix (2/2)

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,12 +6,11 @@ import EntendaOnsurnance from './formulario-onsurance.component'
 import TooltipHelp from './tooltio-help.component'
 import TempoResult from './tempo-result.component'
 import SobreVoce from './formulario-sobre.component'
-import FormularioFim from './formulario-fim'
 import ResultadoFormulario from './resultado-formulario.component'
 import IconeCotacao from './icone-cotacao.component'
 import Footer from './footer.component'
 
 export { 
     Header, CartIcon, Veiculo, Cotacao, EntendaOnsurnance, TooltipHelp, TempoResult, SobreVoce, 
-    FormularioFim, ResultadoFormulario, IconeCotacao, Footer
+    ResultadoFormulario, IconeCotacao, Footer
 }
